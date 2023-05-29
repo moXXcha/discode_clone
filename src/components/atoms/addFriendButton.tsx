71ca7c4fb5727b37e1184c57086b4a22d284c546
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 09:34:47 +0900

    create addFriendButton

---
+++ src/components/atoms/addFriendButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddFriendButton = () => {
+  return (
+    <button className="w-24 h-6 bg-[#248046] rounded-md">
+        <p className="text-xs">フレンドに追加</p>
+    </button>
+  )
+}
+
+export default AddFriendButton
