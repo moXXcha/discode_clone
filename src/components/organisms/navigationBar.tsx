7d2b82c4b6a38ced9f1bdec815b9518c420ee3b0
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 20:23:54 +0900

    fix navigationBar

--- src/components/organisms/navigationBar.tsx
+++ src/components/organisms/navigationBar.tsx
@@ -5,9 +5,14 @@ import PlusButton from "../atoms/plusButton";
 
 const NavigationBar = () => {
   return (
-    <div className="w-28 bg-navigation">
+    <div className="w-28 h-screen bg-navigation flex flex-col items-center pt-2">
+      <div className="mb-3">
       <DiscodeIcon />
+      <label className="h-[1px] w-10 border-b block mx-auto mt-2"></label>
+      </div>
+      <div className="mb-3">
       <ServerIcon src="/ojisan.jpg" />
+      </div>
       <PlusButton />
     </div>
   );
