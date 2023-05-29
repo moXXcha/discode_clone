e10f82c7b75575ca0f1b3f7c122ee78b12d8cc06
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 10:32:06 +0900

    fix css DMButton

--- src/components/atoms/DMButton.tsx
+++ src/components/atoms/DMButton.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const DMButton = () => {
   return (
-    <button className="bg-primary w-4 h-4 rounded-full">
+    <button className="bg-primary w-4 h-4 rounded-full block">
         <Image src="/DM.svg" width="8" height="8" alt="DM" className="mx-auto" />
     </button>
   )
