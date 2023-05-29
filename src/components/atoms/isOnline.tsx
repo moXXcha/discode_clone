03b7da83c297f1b05bb4e7141f7888c6be01e281
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 10:31:18 +0900

    fix css isOnline

--- src/components/atoms/isOnline.tsx
+++ src/components/atoms/isOnline.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const IsOnline = () => {
   return (
-    <p className="text[8px] text-secondText">オンライン</p>
+    <p className="text-[8px] text-secondText">オンライン</p>
   )
 }
 
