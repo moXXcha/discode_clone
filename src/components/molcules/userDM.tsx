a61cdc8cce267c1e4a54c83471598c644c08bd59
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 10:33:34 +0900

    create userDM

---
+++ src/components/molcules/userDM.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import UserIcon from "../atoms/userIcon";
+import DMButton from "../atoms/DMButton";
+import MenuButton from "../atoms/menuButton";
+import UserName from "../atoms/userName";
+import IsOnline from "../atoms/isOnline";
+
+const UserDM = () => {
+  return (
+    <div className="w-5/6 h-10 bg-secondary flex px-2 rounded-md items-center">
+      <div className="flex">
+        <UserIcon src="/ojisan.jpg" />
+        <div className="ml-2">
+          <UserName name="chacha" />
+          <IsOnline />
+        </div>
+      </div>
+      <div className="ml-auto flex">
+        <div className="mr-2">
+        <DMButton />
+        </div>
+        <MenuButton />
+      </div>
+    </div>
+  );
+};
+
+export default UserDM;
