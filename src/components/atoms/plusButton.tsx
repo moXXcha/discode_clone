fc4470e0957898fb605e1050b313648e9d5132c4
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 09:19:42 +0900

    fix css

--- src/components/atoms/plusButton.tsx
+++ src/components/atoms/plusButton.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const PlusButton = () => {
   return (
-    <button className="w-14 h-14 rounded-full bg-[#303338 block">
+    <button className="w-14 h-14 rounded-full bg-[#303338] block">
         <Image src={Plus} width="29" height="29" alt="plus" className="mx-auto"/>
     </button>
   )
