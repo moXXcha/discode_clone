cfc09e71880931fb4ffcd27bbc72226301e948f2
moXXcha <chacha.love.sho@icloud.com>
2023-05-29 20:35:51 +0900

    create searchChannel

---
+++ src/components/atoms/searchChannel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchChannel = () => {
+  return (
+    <div>
+        <input type="text" placeholder='会議を検索する' className="bg-dark w-24 h-5 text-[8px] rounded-md px-1"/>
+    </div>
+  )
+}
+
+export default SearchChannel
