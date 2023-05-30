e69747462785effa8bef61449e4e623866eb449d
moXXcha <chacha.love.sho@icloud.com>
2023-05-30 12:46:34 +0900

    fix width navigationBar

--- src/components/organisms/navigationBar.tsx
+++ src/components/organisms/navigationBar.tsx
@@ -5,7 +5,7 @@ import PlusButton from "../atoms/plusButton";
 
 const NavigationBar = () => {
   return (
-    <div className="w-28 h-screen bg-navigation flex flex-col items-center pt-2">
+    <div className="w-1/12 h-screen bg-navigation flex flex-col items-center pt-2">
       <div className="mb-3">
       <DiscodeIcon />
       <label className="h-[1px] w-10 border-b block mx-auto mt-2"></label>
