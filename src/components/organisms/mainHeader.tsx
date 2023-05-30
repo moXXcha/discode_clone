9e21d314ec58de59b014dd28d4d7c0643af6e11a
moXXcha <chacha.love.sho@icloud.com>
2023-05-30 13:09:33 +0900

    create mainHeader

---
+++ src/components/organisms/mainHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import UtilityButton from "../molecules/utilityButton";
+import SortUserButton from "../atoms/sortUserButton";
+import AddFriendButton from "../atoms/addFriendButton";
+
+const MainHeader = () => {
+  return (
+    <div className="flex w-10/12 border border-border rounded-md">
+      <div className="px-6">
+        <div className="border-r pr-6">
+          <UtilityButton status="friend" />
+        </div>
+      </div>
+      <div className="space-x-6">
+        <SortUserButton status="オンライン" />
+        <SortUserButton status="全て表示" />
+        <SortUserButton status="保留中" />
+        <SortUserButton status="ブロック中" />
+        <AddFriendButton />
+      </div>
+    </div>
+  );
+};
+
+export default MainHeader;
